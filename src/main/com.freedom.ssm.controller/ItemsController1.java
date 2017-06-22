d57086ba02a91bd65ff1e1a68a7aa279b450a7e3
kshine <250270704@qq.com>
2017-06-22 20:57:34 +0800

    Initial commit

---
+++ src/main/com.freedom.ssm.controller/ItemsController1.java
@@ -0,0 +1,12 @@
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * Created by kshine on 2017/6/22.
+ */
+public class ItemsController1 implements Controller {
+
+    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
+        return null;
+    }
+}
